d7c4004614b43c5c970fd9ca15c204c5aa6063c1
Godwyll Aikins <gaikins2015@my.fit.edu>
2023-05-29 15:51:52 -0400

    main (work-in-progress)

---
+++ isaacgymenvs/PPO/main.py
@@ -0,0 +1,77 @@
+import argparse
+import os
+import random
+import time
+
+import gym
+import isaacgym  # noqa
+import isaacgymenvs
+import numpy as np
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from utils import RecordEpisodeStatisticsTorch, ExtractObsWrapper
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument("--env", default="Lando")          		   
+    p.add_argument("--seed", default=0, type=int)
+    p.add_argument("--num_envs", type=int, default=4096)
+    p.add_argument("--rollout_steps", type=int, default=16)
+    p.add_argument("--total_steps", type=int, default=3000000)
+    p.add_argument("--headless", action="store_true")
+    
+    args = p.parse_args()
+    return args
+
+if __name__=="__main__":
+    
+    args = parse_args()
+
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    envs = isaacgymenvs.make(
+        seed=0, 
+        task=args.env, 
+        num_envs=args.num_envs, 
+        sim_device="cuda:0",
+        rl_device="cuda:0",
+        graphics_device_id=-1,
+        headless=True,
+        force_render=True,
+
+    )
+
+    envs = ExtractObsWrapper(envs)
+    envs = RecordEpisodeStatisticsTorch(envs, device)
+    envs.single_action_space = envs.action_space
+    envs.single_observation_space = envs.observation_space
+    assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
+
+    # ALGO Logic: Storage setup
+    obs = torch.zeros((args.rollout_steps, args.num_envs) + envs.single_observation_space.shape, dtype=torch.float).to(device)
+    actions = torch.zeros((args.rollout_steps, args.num_envs) + envs.single_action_space.shape, dtype=torch.float).to(device)
+    logprobs = torch.zeros((args.rollout_steps, args.num_envs), dtype=torch.float).to(device)
+    rewards = torch.zeros((args.rollout_steps, args.num_envs), dtype=torch.float).to(device)
+    dones = torch.zeros((args.rollout_steps, args.num_envs), dtype=torch.float).to(device)
+    values = torch.zeros((args.rollout_steps, args.num_envs), dtype=torch.float).to(device)
+    advantages = torch.zeros_like(rewards, dtype=torch.float).to(device)
+
+    global_step = 0
+    next_obs = envs.reset()
+    next_done = torch.zeros(args.num_envs, dtype=torch.float).to(device)
+
+    for global_step in range(args.total_steps):
+        for step in range(0, args.rollout_steps):
+            global_step += envs.num_envs
+            obs[step] = next_obs
+            dones[step] = next_done
+            actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
+            actions = torch.from_numpy(actions)
+            next_obs, rewards[step], next_done, info = envs.step(actions)
+        
+
+
+        
