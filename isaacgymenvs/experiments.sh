894435006427cdde22bcabc1bf4f1d438380ac04
Godwyll Aikins <gaikins2015@my.fit.edu>
2023-08-21 15:50:48 -0400

    experiments

--- isaacgymenvs/experiments.sh
+++ isaacgymenvs/experiments.sh
@@ -55,6 +55,60 @@ python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.25
 
 clear
 cd ../
+cd PPO-LSTM
+echo "Starting Flicker Experiments"
+python main.py --POMDP=flicker --pomdp_prob=0.1
+python main.py --POMDP=flicker --pomdp_prob=0.2
+python main.py --POMDP=flicker --pomdp_prob=0.3
+python main.py --POMDP=flicker --pomdp_prob=0.4
+python main.py --POMDP=flicker --pomdp_prob=0.5
+
+clear 
+echo "Staring Noise Experiments"
+python main.py --POMDP=random_noise --pomdp_prob=0.05
+python main.py --POMDP=random_noise --pomdp_prob=0.1
+python main.py --POMDP=random_noise --pomdp_prob=0.15
+python main.py --POMDP=random_noise --pomdp_prob=0.2
+python main.py --POMDP=random_noise --pomdp_prob=0.25
+python main.py --POMDP=random_noise --pomdp_prob=0.23
+
+clear 
+echo "Staring Flicker + Noise Experiments"
+python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.05
+python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.1
+python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.15
+python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.2
+python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.25
+
+clear
+cd ../
+cd RPO-LSTM_Critic
+echo "Starting Flicker Experiments"
+python main.py --POMDP=flicker --pomdp_prob=0.1
+python main.py --POMDP=flicker --pomdp_prob=0.2
+python main.py --POMDP=flicker --pomdp_prob=0.3
+python main.py --POMDP=flicker --pomdp_prob=0.4
+python main.py --POMDP=flicker --pomdp_prob=0.5
+
+clear 
+echo "Staring Noise Experiments"
+python main.py --POMDP=random_noise --pomdp_prob=0.05
+python main.py --POMDP=random_noise --pomdp_prob=0.1
+python main.py --POMDP=random_noise --pomdp_prob=0.15
+python main.py --POMDP=random_noise --pomdp_prob=0.2
+python main.py --POMDP=random_noise --pomdp_prob=0.25
+python main.py --POMDP=random_noise --pomdp_prob=0.23
+
+clear 
+echo "Staring Flicker + Noise Experiments"
+python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.05
+python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.1
+python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.15
+python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.2
+python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.25
+
+clear
+cd ../
 cd PPO_Critic
 echo "Starting Flicker Experiments"
 python main.py --POMDP=flicker --pomdp_prob=0.1
@@ -109,33 +163,6 @@ python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.25
 
 clear
 cd ../
-cd PPO-LSTM
-echo "Starting Flicker Experiments"
-python main.py --POMDP=flicker --pomdp_prob=0.1
-python main.py --POMDP=flicker --pomdp_prob=0.2
-python main.py --POMDP=flicker --pomdp_prob=0.3
-python main.py --POMDP=flicker --pomdp_prob=0.4
-python main.py --POMDP=flicker --pomdp_prob=0.5
-
-clear 
-echo "Staring Noise Experiments"
-python main.py --POMDP=random_noise --pomdp_prob=0.05
-python main.py --POMDP=random_noise --pomdp_prob=0.1
-python main.py --POMDP=random_noise --pomdp_prob=0.15
-python main.py --POMDP=random_noise --pomdp_prob=0.2
-python main.py --POMDP=random_noise --pomdp_prob=0.25
-python main.py --POMDP=random_noise --pomdp_prob=0.23
-
-clear 
-echo "Staring Flicker + Noise Experiments"
-python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.05
-python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.1
-python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.15
-python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.2
-python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.25
-
-clear
-cd ../
 cd RPO-LSTM
 echo "Starting Flicker Experiments"
 python main.py --POMDP=flicker --pomdp_prob=0.1
@@ -161,29 +188,3 @@ python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.15
 python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.2
 python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.25
 
-clear
-cd ../
-cd RPO-LSTM_Critic
-echo "Starting Flicker Experiments"
-python main.py --POMDP=flicker --pomdp_prob=0.1
-python main.py --POMDP=flicker --pomdp_prob=0.2
-python main.py --POMDP=flicker --pomdp_prob=0.3
-python main.py --POMDP=flicker --pomdp_prob=0.4
-python main.py --POMDP=flicker --pomdp_prob=0.5
-
-clear 
-echo "Staring Noise Experiments"
-python main.py --POMDP=random_noise --pomdp_prob=0.05
-python main.py --POMDP=random_noise --pomdp_prob=0.1
-python main.py --POMDP=random_noise --pomdp_prob=0.15
-python main.py --POMDP=random_noise --pomdp_prob=0.2
-python main.py --POMDP=random_noise --pomdp_prob=0.25
-python main.py --POMDP=random_noise --pomdp_prob=0.23
-
-clear 
-echo "Staring Flicker + Noise Experiments"
-python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.05
-python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.1
-python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.15
-python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.2
-python main.py --POMDP=flickering_and_random_noise --pomdp_prob=0.25
